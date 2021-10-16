ed75fe290bd515c433703b9e0d2f23bd325843ae
Ajani Timothy <timothyinioluwa@gmail.com>
2021-10-16 09:06:45 +0100

    first commit

--- search_engine/settings.py
+++ search_engine/settings.py
@@ -2,7 +2,11 @@ import os
 from pathlib import Path
 import django_heroku
 import dj_database_url
+import environ
 
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -12,10 +16,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=(3!gjwz5@%)j8i_ucfgldwrfg!0zenshby#2#@capuhru4vp7'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
